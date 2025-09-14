e052a1b367d300091d8ee625cbfd6a7f5c515150
Keira <keirajames2003@gmail.com>
2025-09-14 00:17:42 -0400

    added shell tools module for running shell tools

---
+++ modules/tools/shell_tools.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def run_command(command: str) -> str:
+    
+    try:
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        return result.stdout.strip()
+    
+    except subprocess.CalledProcessError as e:
+        return "Error: ", {e}
